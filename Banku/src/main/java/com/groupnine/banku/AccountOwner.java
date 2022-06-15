5e2130a95d5fc997f3675d6a992149da3f29a2a2
Carlos Monteiro <97047838+Carlos-MVM@users.noreply.github.com>
2022-06-15 16:48:44 -0100

    Class implementation: - AccOwner - EAccOwner - EPartner - PAccOwner

---
+++ Banku/src/main/java/com/groupnine/banku/AccountOwner.java
@@ -0,0 +1,38 @@
+package com.groupnine.banku;
+
+public class AccountOwner {
+    private String name;
+    private String NIF;
+    private String address;
+
+
+    public AccountOwner(String name, String NIF, String address) {
+        this.name = name;
+        this.NIF = NIF;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNIF() {
+        return NIF;
+    }
+
+    public void setNIF(String NIF) {
+        this.NIF = NIF;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
