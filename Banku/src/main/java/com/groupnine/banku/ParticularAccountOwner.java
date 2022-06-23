e0e9255c26d0006d6cf08b1cebe2e2492d03be98
Carlos Monteiro <97047838+Carlos-MVM@users.noreply.github.com>
2022-06-23 16:13:12 -0100

    add all function for the class ParticularAccountOwner

--- Banku/src/main/java/com/groupnine/banku/ParticularAccountOwner.java
+++ Banku/src/main/java/com/groupnine/banku/ParticularAccountOwner.java
@@ -1,21 +1,44 @@
 package com.groupnine.banku;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ParticularAccountOwner  extends AccountOwner{
      private String lastName;
      private String id;
      private String nationality;
-     private ArrayList<ParticularsAccount> accounts;
-     private ArrayList<OrdinaryParticularAccount> associatedAccounts;
+     private ArrayList<Account> accounts;
+     private ArrayList<Account> associatedAccounts;
 
-     public ParticularAccountOwner(String name, String NIF, String address, String lastName, String id, String nationality) {
+     public ParticularAccountOwner(String name, String NIF, String address, String lastname, String id, String nationality,
+                                   ArrayList<? extends Account>accounts, ArrayList<? extends Account>associatedAccounts ){
           super(name, NIF, address);
-          this.lastName = lastName;
+          this.lastName = lastname;
           this.id = id;
           this.nationality = nationality;
-          this.accounts = new ArrayList<>();
-          this.associatedAccounts = new ArrayList<>();
+          this.accounts = (ArrayList<Account>) accounts;
+          this.associatedAccounts = (ArrayList<Account>) associatedAccounts;
+     }
+
+     public ParticularAccountOwner(String name, String NIF, String address, String lastName, String id, String nationality,
+                                   ArrayList<? extends Account>accounts ) {
+          this(name, NIF, address, lastName, id, nationality, accounts, new ArrayList<>() );
+     }
+
+     public ParticularAccountOwner(String name, String NIF, String address, String lastName, String id, String nationality) {
+          this(name, NIF, address, lastName, id, nationality, new ArrayList<>(), new ArrayList<>() );
+     }
+
+     public Double getTotalAvailableBalance(){
+          Double balance = 0d;
+          for (int i = 0; i < accounts.size(); i++){
+               balance += accounts.get(i).getAccountBalance();
+          }
+          return balance;
+     }
+
+     public void addAccount(Account account){
+          accounts.add(account);
      }
 
      public String getLastName() {
