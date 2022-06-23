8639b4436ec7a8834070e14f34ba10cd591955d5
anaximeno <anaximenobrito@gmail.com>
2022-06-23 16:09:55 -0100

    fix: Fixed some implementations

--- Banku/src/main/java/com/groupnine/banku/ParticularAccount.java
+++ Banku/src/main/java/com/groupnine/banku/ParticularAccount.java
@@ -3,7 +3,8 @@ package com.groupnine.banku;
 public abstract class ParticularAccount extends Account{
     private ParticularAccountOwner owner;
 
-    public ParticularAccount(ParticularAccountOwner owner) {
+    public ParticularAccount(String accountName, String accountNumber, Double availableBalance, ParticularAccountOwner owner, Double interestRate) {
+        super(accountNumber, accountName, availableBalance, interestRate);
         this.owner = owner;
     }
 
