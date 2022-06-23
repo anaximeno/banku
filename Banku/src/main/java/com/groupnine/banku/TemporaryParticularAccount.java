2e8b3234620a5c1c80300786bc026cfacb311c15
anaximeno <anaximenobrito@gmail.com>
2022-06-23 16:40:41 -0100

    fix: Made some fixes
    also: feat: Added some methods implementations

--- Banku/src/main/java/com/groupnine/banku/TemporaryParticularAccount.java
+++ Banku/src/main/java/com/groupnine/banku/TemporaryParticularAccount.java
@@ -1,11 +1,38 @@
 package com.groupnine.banku;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.Period;
+import java.lang.Math;
 
-public class TemporaryParticularAccount {
-    private LocalDateTime creationDate;
-    private LocalDateTime expirationDate;
-    private Boolean boost;
+public class TemporaryParticularAccount extends ParticularAccount {
+    private LocalDate creationDate;
+    private LocalDate expirationDate;
+    private boolean boost;
+
+    public TemporaryParticularAccount(String accountNumber, String accountName, Double balance,
+        Double interestRate, ParticularAccountOwner owner, LocalDate creationDate, LocalDate expirationDate) {
+        super(accountName, accountNumber, balance, owner, interestRate);
+        this.creationDate = creationDate;
+        this.expirationDate = expirationDate;
+    }
+
+    public TemporaryParticularAccount(String accountNumber, String accountName, Double balance,
+          Double interestRate, ParticularAccountOwner owner, LocalDate creationDate, LocalDate expirationDate, boolean boost) {
+        this(accountNumber, accountName, balance, interestRate, owner, creationDate, expirationDate);
+        this.boost = boost;
+    }
+
+    public boolean hasBoost() {
+        return boost;
+    }
+
+    public void activateBoost() {
+        this.boost = true;
+    }
+
+    public void deactivateBoost() {
+        this.boost = false;
+    }
 
     public LocalDate getCreationDate() {
         return creationDate;
@@ -17,5 +44,9 @@ public class TemporaryParticularAccount {
 
     public void setExpirationDate(LocalDate expirationDate) {
         this.expirationDate = expirationDate;
+    }
+
+    public Period getNumberOfDaysToExpiration() {
+        return Period.between(creationDate, expirationDate);
     }
 }
