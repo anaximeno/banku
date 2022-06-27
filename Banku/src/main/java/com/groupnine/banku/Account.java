c72515df3cb443aee731ddfb1bce90ccf7b356a1
Sophie_24 <sophieevora24@gmail.com>
2022-06-27 12:00:56 -0100

    Merge branch 'tams' of C:\Users\Tamiris Évora\OneDrive - Universidade de Cabo Verde\2nd year\2nd semester\Programação Orientada a Objetos\Projeto POO\9_sistemas_de_gestao_de_contas_bancarias with conflicts.

--- Banku/src/main/java/com/groupnine/banku/Account.java
+++ Banku/src/main/java/com/groupnine/banku/Account.java
@@ -1,47 +1,53 @@
 package com.groupnine.banku;
 
-
 public abstract class Account {
     private String accountNumber;
     private String accountName;
     private Double availableBalance;
     private Double interestRate;
 
-    public Account(String accountNumber, String accountName, Double availableBalance, Double interestRate) {
+    public Account(String accountNumber, String accountName, Double availableBalance) {
         this.accountNumber = accountNumber;
         this.accountName = accountName;
         this.availableBalance = availableBalance;
-        this.interestRate = interestRate;
     }
 
     public String getAccountNumber() {
+        
         return accountNumber;
     }
 
     public void setAccountNumber(String accountNumber) {
+        
         this.accountNumber = accountNumber;
     }
 
     public String getAccountName() {
+        
         return accountName;
     }
 
     public void setAccountName(String accountName) {
+        
         this.accountName = accountName;
     }
 
     public Double getAccountBalance() {
+        
         return availableBalance;
     }
 
     public void setAccountBalance(Double balance) {
+        
         this.availableBalance = balance;
     }
 
     public void updateInterestRateTo(Double interestRate){
-        this.interestRate = interestRate;
+
     }
     public Double getInterestRate(){
+
         return interestRate;
     }
+
 }
--- Banku/src/main/java/com/groupnine/banku/EnterpriseAccount.java
+++ Banku/src/main/java/com/groupnine/banku/EnterpriseAccount.java
@@ -1,72 +1,15 @@
 package com.groupnine.banku;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class EnterpriseAccount extends Account{
-    private ArrayList<Card> cards;
+public class EnterpriseAccount {
+    private List<Card> cards;
     private ParticularAccountOwner admin;
-    private ArrayList<ParticularAccountOwner> authorizedUsers;
-    private ArrayList<Double> dailyBalanceRecordOfTheMonth;
-    private boolean todayBalanceWasSaved;
+    private List<ParticularAccountOwner> authorizedUsers;
+    private List<Double> dailyBalanceRecordOfTheMonth;
+    private Boolean todaysBalanceWasSaved;
 
-    public EnterpriseAccount(String accountNumber, String accountName, Double availableBalance, Double interestRate,
-                             ArrayList<Card> cards, ParticularAccountOwner admin, ArrayList<ParticularAccountOwner> authorizedUsers) {
-        super(accountNumber, accountName, availableBalance, interestRate);
-        this.cards = cards;
-        this.admin = admin;
-        this.authorizedUsers = authorizedUsers;
-        this.dailyBalanceRecordOfTheMonth = new ArrayList<>();
-        this.todayBalanceWasSaved = false;
-    }
-
-    public Double getAverageMonthlyBalance(){
-        double average = 0d;
-        if (dailyBalanceRecordOfTheMonth.size() != 0) {
-            for (int i = 0; i < dailyBalanceRecordOfTheMonth.size(); i++) {
-                average += dailyBalanceRecordOfTheMonth.get(i).doubleValue();
-            }
-            average /= dailyBalanceRecordOfTheMonth.size();
-        }
-        return  average;
-    }
-
-    public ArrayList<Card> getCards() {
-        return cards;
-    }
-
-    public void setCards(ArrayList<Card> cards) {
-        this.cards = cards;
-    }
-
-    public ParticularAccountOwner getAdmin() {
-        return admin;
-    }
-
-    public void setAdmin(ParticularAccountOwner admin) {
-        this.admin = admin;
-    }
-
-    public ArrayList<ParticularAccountOwner> getAuthorizedUsers() {
-        return authorizedUsers;
-    }
-
-    public void setAuthorizedUsers(ArrayList<ParticularAccountOwner> authorizedUsers) {
-        this.authorizedUsers = authorizedUsers;
-    }
-
-    public ArrayList<Double> getDailyBalanceRecordOfTheMonth() {
+    public List<Double> getAverageMonthlyBalance(){
         return dailyBalanceRecordOfTheMonth;
-    }
-
-    public void setDailyBalanceRecordOfTheMonth(ArrayList<Double> dailyBalanceRecordOfTheMonth) {
-        this.dailyBalanceRecordOfTheMonth = dailyBalanceRecordOfTheMonth;
-    }
-
-    public boolean isTodayBalanceWasSaved() {
-        return todayBalanceWasSaved;
-    }
-
-    public void setTodayBalanceWasSaved(boolean todayBalanceWasSaved) {
-        this.todayBalanceWasSaved = todayBalanceWasSaved;
     }
 }
