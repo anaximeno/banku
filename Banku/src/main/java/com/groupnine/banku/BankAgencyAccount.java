26b2a6253af64258ae71f810edf495b883ec39e0
anaximeno <anaximenobrito@gmail.com>
2022-07-05 18:28:48 -0100

    feat: Add BankAgencyAccount

---
+++ Banku/src/main/java/com/groupnine/banku/BankAgencyAccount.java
@@ -0,0 +1,31 @@
+package com.groupnine.banku;
+
+import java.util.*;
+
+public class BankAgencyAccount {
+    private Employee admin;
+    private List<Employee> authorizedUsers;
+    private double balance;
+
+    public BankAgencyAccount(Employee admin, double balance) {
+        this.admin = admin;
+        this.balance = balance;
+        // TODO: handle authorized users
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalanceTo(double balance) {
+        this.balance = balance;
+    }
+
+    public Employee getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Employee admin) {
+        this.admin = admin;
+    }
+}
