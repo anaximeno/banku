b11d2c8026c9cb08ba773555227db64860d4e8da
anaximeno <anaximenobrito@gmail.com>
2022-07-05 21:27:56 -0100

    feat: Add owner

--- Banku/src/main/java/com/groupnine/banku/Account.java
+++ Banku/src/main/java/com/groupnine/banku/Account.java
@@ -5,12 +5,22 @@ public abstract class Account {
     private String accountName;
     private Double availableBalance;
     private Double interestRate;
+    private AccountOwner owner;
 
-    public Account(String accountNumber, String accountName, Double availableBalance, Double interestRate) {
+    public Account(String accountNumber, String accountName, AccountOwner owner, Double availableBalance, Double interestRate) {
         this.accountNumber = accountNumber;
         this.accountName = accountName;
         this.availableBalance = availableBalance;
         this.interestRate = interestRate;
+        this.owner = owner;
+    }
+
+    public AccountOwner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(ParticularAccountOwner owner) {
+        this.owner = owner;
     }
 
     public String getAccountNumber() {
