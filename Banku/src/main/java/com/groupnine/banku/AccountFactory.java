d7bc60d574065fe7c06371a7e5c5e5f4c652b24a
Carlos Monteiro <97047838+Carlos-MVM@users.noreply.github.com>
2022-07-05 23:15:22 -0100

    Modified AccountFactory : a little changes

--- Banku/src/main/java/com/groupnine/banku/AccountFactory.java
+++ Banku/src/main/java/com/groupnine/banku/AccountFactory.java
@@ -1,10 +1,20 @@
 package com.groupnine.banku;
 
 import java.lang.Math;
+import java.util.ArrayList;
 
 public class AccountFactory {
     private static int accountNumberCounter = 0;
+    private static double currentEnterpriseInterestRate;
+    private static double InterestRate;
+    private static double currentTemporaryAccountInterestRate (){
 
+    }
+
+    public static EnterpriseAccount createEnterpriseAccount (String name, ParticularAccountOwner admin,
+                                                             double initialBalance, ArrayList<ParticularAccountOwner> associated){
+
+    }
     private static void increaseCounter() {
         accountNumberCounter += 1;
     }
