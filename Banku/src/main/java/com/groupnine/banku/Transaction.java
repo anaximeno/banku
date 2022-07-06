6263d7be811d534b41789919dd06fd0929fb1bfd
anaximeno <anaximenobrito@gmail.com>
2022-07-06 02:29:25 -0100

    refactor: Made some refactoring

--- Banku/src/main/java/com/groupnine/banku/Transaction.java
+++ Banku/src/main/java/com/groupnine/banku/Transaction.java
@@ -13,31 +13,36 @@ public class Transaction extends BankingOperation{
     private double balanceOfToAfterTransaction;
 
     public String getDescription (){
-        String description = "Money transaction from account with number" +this.accountFrom.getAccountNumber()
-                + " to account with number " + this.accountTo.getAccountNumber() + " in the amount of " +this.value;
+        String description = "Money transaction from account with number" + accountFrom.getAccountNumber()
+                + " to account with number " + accountTo.getAccountNumber() + " in the amount of " + value;
          return description;
     }
 
     public String getFullDescription (){
-        String fullDescription = "Money transaction from account with number" + this.accountFrom.getAccountNumber()
-                + " to account with number " + this.accountTo.getAccountNumber() + " in the amount of " +this.value
-                + "\n Account balance debited before the transaction = " + this.balanceOfFromAccountBeforeTransaction
-                + "\n Account balance debited after the transaction = " + this.balanceOfFromAccountAfterTransaction
-                + "\n Account balance credited before the transaction = " + this.balanceOfToAccountBeforeTransaction
-                + "\n Account balance credited after the transaction = " + this.balanceOfToAfterTransaction;
+        String fullDescription = getDescription()
+                + "\n Account balance debited before the transaction = " + balanceOfFromAccountBeforeTransaction
+                + "\n Account balance debited after the transaction = " + balanceOfFromAccountAfterTransaction
+                + "\n Account balance credited before the transaction = " + balanceOfToAccountBeforeTransaction
+                + "\n Account balance credited after the transaction = " + balanceOfToAfterTransaction;
         return fullDescription;
 
     }
 
     public Transaction(Operator operator, LocalDateTime dateTime, Account accountFrom, Account accountTo, double value,
-                       double balanceOfFromAccountBeforeTransaction, double balanceOfToAccountBeforeTransaction) {
+                       double balanceOfFromAccountBeforeTransaction, double balanceOfToAccountBeforeTransaction
+    ) {
         super(operator, dateTime);
         this.accountFrom = accountFrom;
         this.accountTo = accountTo;
         this.value = value;
         this.balanceOfFromAccountBeforeTransaction = balanceOfFromAccountBeforeTransaction;
-        this.balanceOfFromAccountAfterTransaction = this.balanceOfFromAccountBeforeTransaction - this.value;
+        double valueWithInterests = value;
+        if (accountFrom instanceof EnterpriseAccount) {
+            valueWithInterests *= (1 + EnterpriseAccount.TRANSACTION_INTEREST);
+        }
+        // Interest is only applied to account from which are EnterpriseAccounts
+        this.balanceOfFromAccountAfterTransaction = balanceOfFromAccountBeforeTransaction - valueWithInterests;
         this.balanceOfToAccountBeforeTransaction = balanceOfToAccountBeforeTransaction;
-        this.balanceOfToAfterTransaction = this.balanceOfToAccountBeforeTransaction + this.value;
+        this.balanceOfToAfterTransaction = balanceOfToAccountBeforeTransaction + value;
     }
 }
