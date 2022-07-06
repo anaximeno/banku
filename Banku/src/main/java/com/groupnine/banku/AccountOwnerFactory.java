5c9360e265fe7a9fa8e0002ecb0d64cc69154f2a
Carlos Monteiro <97047838+Carlos-MVM@users.noreply.github.com>
2022-07-06 11:46:22 -0100

    Modified AccountOwnerFactory

--- Banku/src/main/java/com/groupnine/banku/AccountOwnerFactory.java
+++ Banku/src/main/java/com/groupnine/banku/AccountOwnerFactory.java
@@ -3,31 +3,40 @@ package com.groupnine.banku;
 import java.util.ArrayList;
 
 public class AccountOwnerFactory {
-    public EnterpriseAccount createEnterpriseAccountOwner (String name, String NIF, String address,
+
+    //public AccountOwner createAccount ()
+    public EnterpriseAccountOwner createEnterpriseAccountOwner (String name, String NIF, String address,
                                                            ArrayList<EnterprisePartners> partners,
-                                                           ArrayList<ParticularAccountOwner> authorizedUsers){
-
+                                                           ArrayList<EnterpriseAccount> accounts){
+        EnterpriseAccountOwner eAOwner = new EnterpriseAccountOwner(name, NIF, address, partners, accounts);
+        return eAOwner;
     }
 
-    public EnterpriseAccount createEnterpriseAccountOwner (String name, String NIF, String address,
+    public EnterpriseAccountOwner createEnterpriseAccountOwner (String name, String NIF, String address,
                                                            ArrayList<EnterprisePartners> partners){
-
+        EnterpriseAccountOwner eAOwner =  new EnterpriseAccountOwner(name, NIF, address, partners);
+        return eAOwner;
     }
 
-    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address,
-                                                                String nationality, ArrayList<Account> accounts,
+    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastname,
+                                                                String id, String nationality, ArrayList<Account> accounts,
                                                                 ArrayList<Account> associatedAccounts){
+        ParticularAccountOwner pAOwner = new ParticularAccountOwner(name, NIF, address, lastname, id, nationality,
+                accounts, associatedAccounts);
+        return  pAOwner;
+    }
+
+    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastname,
+                                                                String id, String nationality, ArrayList<Account> accounts){
+        ParticularAccountOwner pAOwner = new ParticularAccountOwner(name, NIF, address, lastname, id, nationality, accounts);
+        return pAOwner;
 
     }
 
-    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address,
-                                                                String nationality, ArrayList<Account> accounts){
-
-    }
-
-    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address,
-                                                                String nationality){
-
+    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastName,
+                                                                String id, String nationality){
+        ParticularAccountOwner pAOwner = new ParticularAccountOwner(name, NIF, address, lastName, id, nationality);
+        return pAOwner;
     }
 
 }
