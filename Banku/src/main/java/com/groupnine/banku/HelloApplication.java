e1c41d36cd6d689f7e46aa9b75e2683a01994992
anaximeno <anaximenobrito@gmail.com>
2022-07-10 15:39:11 -0100

    fix: Fix merge conflicts

--- Banku/src/main/java/com/groupnine/banku/HelloApplication.java
+++ Banku/src/main/java/com/groupnine/banku/HelloApplication.java
@@ -10,17 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< Updated upstream
         FXMLLoader fxmlLoader = new FXMLLoader(
                 HelloApplication.class.getResource("hello-view.fxml")
         );
         Scene scene = new Scene(fxmlLoader.load(), 720, 480);
         stage.setTitle("Banku - test - Banking Application");
-=======
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
->>>>>>> Stashed changes
         stage.setScene(scene);
         stage.show();
     }
