ea6f3805fb6d8e21935958155b5001ddcf12783d
anaximeno <anaximenobrito@gmail.com>
2022-07-10 15:21:17 -0100

    refactor: Made some refactoring and fixes

--- Banku/src/main/java/com/groupnine/banku/Account.java
+++ Banku/src/main/java/com/groupnine/banku/Account.java
@@ -7,57 +7,52 @@ public abstract class Account {
     private Double interestRate;
     private AccountOwner owner;
 
-    public Account(String accountNumber, String accountName, AccountOwner owner, Double availableBalance, Double interestRate) {
-        this.accountNumber = accountNumber;
-        this.accountName = accountName;
-        this.availableBalance = availableBalance;
-        this.interestRate = interestRate;
-        this.owner = owner;
+    public Account(
+            String accountNumber, String accountName, AccountOwner owner, Double availableBalance, Double interestRate
+    ) {
+        setAccountNumber(accountNumber);
+        setAccountName(accountName);
+        setAccountBalance(availableBalance);
+        updateInterestRateTo(interestRate);
+        setOwner(owner);
     }
 
     public AccountOwner getOwner() {
         return owner;
     }
 
-    public void setOwner(ParticularAccountOwner owner) {
+    public void setOwner(AccountOwner owner) {
         this.owner = owner;
     }
 
     public String getAccountNumber() {
-        
         return accountNumber;
     }
 
     public void setAccountNumber(String accountNumber) {
-        
         this.accountNumber = accountNumber;
     }
 
     public String getAccountName() {
-        
         return accountName;
     }
 
     public void setAccountName(String accountName) {
-        
         this.accountName = accountName;
     }
 
     public Double getAccountBalance() {
-        
         return availableBalance;
     }
 
     public void setAccountBalance(Double balance) {
-        
         this.availableBalance = balance;
     }
 
     public void updateInterestRateTo(Double interestRate){
-
+        this.interestRate = interestRate;
     }
     public Double getInterestRate(){
-
         return interestRate;
     }
 
