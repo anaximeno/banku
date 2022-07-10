0c065622e8d7375c299a18dc25d8230b870f785f
anaximeno <anaximenobrito@gmail.com>
2022-07-10 15:20:40 -0100

    refactor: Made some refactoring of the code

--- Banku/src/main/java/com/groupnine/banku/AccountFactory.java
+++ Banku/src/main/java/com/groupnine/banku/AccountFactory.java
@@ -6,31 +6,33 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AccountFactory {
+// This class is abstract because only one AccountFactory will be used for all accounts
+// so there's no need for instantiation on this class
+public abstract class AccountFactory {
     private static int accountNumberCounter = 0;
     private static double currentEnterpriseInterestRate;
     private static double currentOrdinaryAccountInterestRate;
     private static double currentTemporaryAccountInterestRate;
 
-    public static EnterpriseAccount createEnterpriseAccount (String name, EnterpriseAccountOwner owner ,ParticularAccountOwner admin,
-                                                             double initialBalance){
-        EnterpriseAccount eAccount = new EnterpriseAccount (getNewAccountNumberEncoded(), name, owner, admin,
-        initialBalance, currentEnterpriseInterestRate);
-        return eAccount;
+    public static EnterpriseAccount createEnterpriseAccount(
+            String name, EnterpriseAccountOwner owner, ParticularAccountOwner admin, double initialBalance) {
+        String accountNumber = getNewAccountNumberEncoded();
+        double interest = currentEnterpriseInterestRate;
+        return new EnterpriseAccount(accountNumber, name, owner, admin, initialBalance, interest);
     }
 
-    public OrdinaryParticularAccount createOrdinaryParticularAccount (String name, ParticularAccountOwner owner, double balance,
-                                                                      ParticularAccountOwner associate){
-        OrdinaryParticularAccount oAccount = new OrdinaryParticularAccount(getNewAccountNumberEncoded(), name, owner, balance,
-                currentOrdinaryAccountInterestRate);
-        return  oAccount;
+    public static OrdinaryParticularAccount createOrdinaryParticularAccount(
+            String name, ParticularAccountOwner owner, double balance, ParticularAccountOwner associate) {
+        String accountNumber = getNewAccountNumberEncoded();
+        double interest = currentOrdinaryAccountInterestRate;
+        return new OrdinaryParticularAccount(accountNumber, name, owner, balance, interest);
     }
     
-    public TemporaryParticularAccount createTemporaryParticularAccount (String accountName, Double balance,
-                                                                        ParticularAccountOwner owner, LocalDate expirationDate){
-        TemporaryParticularAccount tAccount = new TemporaryParticularAccount(getNewAccountNumberEncoded(), accountName, balance,
-               currentTemporaryAccountInterestRate, owner, LocalDate.now(), expirationDate);
-        return  tAccount;
+    public static TemporaryParticularAccount createTemporaryParticularAccount(
+            String accountName, Double balance, ParticularAccountOwner owner, LocalDate expirationDate) {
+        String accountNumber = getNewAccountNumberEncoded();
+        double interest = currentTemporaryAccountInterestRate;
+        return  new TemporaryParticularAccount(accountNumber, accountName, balance, interest, owner, LocalDate.now(), expirationDate);
     }
     private static void increaseCounter() {
         accountNumberCounter += 1;
@@ -39,18 +41,15 @@ public class AccountFactory {
     private static String getNewAccountNumberEncoded() {
         String num = String.valueOf(accountNumberCounter);
 
-        int x = accountNumberCounter;
+        // accu - is the accumulator, it accumulates the number of decimal places
+        // which is then reduced until the value from accu is less than the accountNumberCounter
         double accu = Math.pow(10d, 12d);
-
-        // Pad the num with zeros
-        while (x < accu) {
+        for (int x = accountNumberCounter ; x < accu ; accu = accu / 10) {
             num = "0" + num;
-            accu = accu / 10;
         }
 
-        // Increase to counter
+        // Increase the counter
         increaseCounter();
-
         return num;
     }
 }
