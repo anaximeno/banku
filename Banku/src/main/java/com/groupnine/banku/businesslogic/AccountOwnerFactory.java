9eda9c95233bbc6d1ee1f69c70380887898d74f7
anaximeno <anaximenobrito@gmail.com>
2022-07-14 19:11:15 -0100

    fix: Set methods to static

--- Banku/src/main/java/com/groupnine/banku/businesslogic/AccountOwnerFactory.java
+++ Banku/src/main/java/com/groupnine/banku/businesslogic/AccountOwnerFactory.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 public class AccountOwnerFactory {
 
     //public AccountOwner createAccount ()
-    public EnterpriseAccountOwner createEnterpriseAccountOwner (String name, String NIF, String address,
+    public static EnterpriseAccountOwner createEnterpriseAccountOwner (String name, String NIF, String address,
                                                            ArrayList<EnterprisePartners> partners,
                                                            ArrayList<EnterpriseAccount> accounts){
         EnterpriseAccountOwner eAOwner = new EnterpriseAccountOwner(name, NIF, address, partners, accounts);
         return eAOwner;
     }
 
-    public EnterpriseAccountOwner createEnterpriseAccountOwner (String name, String NIF, String address,
+    public static EnterpriseAccountOwner createEnterpriseAccountOwner (String name, String NIF, String address,
                                                            ArrayList<EnterprisePartners> partners){
         EnterpriseAccountOwner eAOwner =  new EnterpriseAccountOwner(name, NIF, address, partners);
         return eAOwner;
     }
 
-    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastname,
+    public static ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastname,
                                                                 String id, String nationality, ArrayList<Account> accounts,
                                                                 ArrayList<Account> associatedAccounts){
         ParticularAccountOwner pAOwner = new ParticularAccountOwner(name, NIF, address, lastname, id, nationality,
@@ -26,14 +26,14 @@ public class AccountOwnerFactory {
         return  pAOwner;
     }
 
-    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastname,
+    public static ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastname,
                                                                 String id, String nationality, ArrayList<Account> accounts){
         ParticularAccountOwner pAOwner = new ParticularAccountOwner(name, NIF, address, lastname, id, nationality, accounts);
         return pAOwner;
 
     }
 
-    public ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastName,
+    public static ParticularAccountOwner createParticularAccountOwner (String name, String NIF, String address, String lastName,
                                                                 String id, String nationality){
         ParticularAccountOwner pAOwner = new ParticularAccountOwner(name, NIF, address, lastName, id, nationality);
         return pAOwner;
