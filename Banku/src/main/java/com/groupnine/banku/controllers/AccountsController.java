e4f8ceb2bf67056531c37deaaf13955d79fcf0b1
anaximeno <anaximenobrito@gmail.com>
2022-07-14 18:32:46 -0100

    feat: Update Accounts view

--- Banku/src/main/java/com/groupnine/banku/controllers/AccountsController.java
+++ Banku/src/main/java/com/groupnine/banku/controllers/AccountsController.java
@@ -2,12 +2,15 @@ package com.groupnine.banku.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 
 import java.io.IOException;
 
 public class AccountsController {
     @FXML
     private Button dashboardBtn;
+    @FXML
+    private TableView particularAccountsTable;
 
     @FXML
     protected void dashboardBtnOnClick() throws IOException {
