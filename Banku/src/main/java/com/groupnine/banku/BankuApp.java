45bda3031dedee33db4cd7f99b6bfa3caca088a1
anaximeno <anaximenobrito@gmail.com>
2022-07-16 23:22:34 -0100

    test: Add enterprise account to the BankAgency instance for tests

--- Banku/src/main/java/com/groupnine/banku/BankuApp.java
+++ Banku/src/main/java/com/groupnine/banku/BankuApp.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class BankuApp extends Application {
     @Override
@@ -22,11 +23,8 @@ public class BankuApp extends Application {
         IOperator operator = agency.getBankOperator("John", "Doe", "isDoe");
 
         if (operator != null) {
-            ParticularAccountOwner owner1 = AccountOwnerFactory.createParticularAccountOwner(
-                    "Jane", "324413131", "Praia, Cabo Verde", "Adina Freire", "0012e", "Cape Berdianu");
-            ParticularAccountOwner owner2 = AccountOwnerFactory.createParticularAccountOwner(
-                    "Francis", "320013131", "Tarrafal, Santiago, Cabo Verde", "Sanchu Neto", "0034e", "South African");
-
+            ParticularAccountOwner owner1 = AccountOwnerFactory.createParticularAccountOwner("Jane", "324413131", "Praia, Cabo Verde", "Adina Freire", "0012e", "Cape Berdianu");
+            ParticularAccountOwner owner2 = AccountOwnerFactory.createParticularAccountOwner("Francis", "320013131", "Tarrafal, Santiago, Cabo Verde", "Sanchu Neto", "0034e", "South African");
             OrdinaryParticularAccount acc1 = AccountFactory.createOrdinaryParticularAccount("Ordiacc", owner1, 100000, owner2);
 
             acc1.addCard(owner1, "001");
@@ -35,6 +33,10 @@ public class BankuApp extends Application {
             operator.addNewAccountToTheBank(acc1);
             operator.addNewAccountToTheBank(AccountFactory.createOrdinaryParticularAccount("myOrdAcc", owner1, 230004, null));
             operator.addNewAccountToTheBank(AccountFactory.createOrdinaryParticularAccount("Foster", owner2, 12000, null));
+
+            EnterpriseAccountOwner eOwner = AccountOwnerFactory.createEnterpriseAccountOwner("LuckeLuke CO", "234124124", "The Place", new ArrayList<>());
+
+            operator.addNewAccountToTheBank(AccountFactory.createEnterpriseAccount("LuckeLukeAcc01", eOwner, owner2, 10000000));
         }
     }
 
