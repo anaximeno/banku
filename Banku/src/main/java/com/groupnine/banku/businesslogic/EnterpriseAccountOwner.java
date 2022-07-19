30c64415275770304779aae04a808049d299c391
anaximeno <anaximenobrito@gmail.com>
2022-07-19 13:37:03 -0100

    fix: made some fixes on the code structure and logic

--- Banku/src/main/java/com/groupnine/banku/businesslogic/EnterpriseAccountOwner.java
+++ Banku/src/main/java/com/groupnine/banku/businesslogic/EnterpriseAccountOwner.java
@@ -1,23 +1,42 @@
 package com.groupnine.banku.businesslogic;
 
+import com.groupnine.banku.miscellaneous.Misc;
+
 import java.util.ArrayList;
 
-public class EnterpriseAccountOwner extends AccountOwner{
-    private ArrayList<EnterprisePartners> partners;
-    private ArrayList<EnterpriseAccount> accounts;
+public class EnterpriseAccountOwner extends AccountOwner {
+    final private ArrayList<EnterprisePartner> partners;
+    final private ArrayList<EnterpriseAccount> accounts;
 
-    //get a list of accounts that the Enterprise own
-    public ArrayList<EnterpriseAccount> getAccounts(){
+    public EnterpriseAccountOwner(String ownerID, String name, String NIF, String address, ArrayList<EnterprisePartner> partners, ArrayList<EnterpriseAccount> accounts) {
+        super(ownerID, name, NIF, address);
+        this.partners = partners;
+        this.accounts = accounts;
+    }
+
+    public EnterpriseAccountOwner(String  ownerID, String name, String NIF, String address, ArrayList<EnterprisePartner> partners) {
+        this(ownerID, name, NIF, address, partners, new ArrayList<EnterpriseAccount>());
+    }
+    
+    public EnterpriseAccountOwner(String ownerID, String name, String NIF, String address) {
+        super(ownerID, name, NIF, address);
+        this.partners = new ArrayList<>();
+        this.accounts = new ArrayList<>();
+    }
+
+
+    //get a list of accounts the Enterprise own
+    public ArrayList<EnterpriseAccount> getAccounts() {
         return this.accounts;
     }
 
     //add an account to the Enterprise list accounts
-    public void addAccount (EnterpriseAccount account){
+    public void addAccount (EnterpriseAccount account) {
         this.accounts.add(account);
     }
 
     //remove an account from the Enterprise list accounts
-    public boolean removeAccount (String accountNumber){
+    public boolean removeAccount (String accountNumber) {
         boolean result = false;
         for (int i = 0; i < accounts.size(); i++){
             if (accounts.get(i).getAccountNumber().equals(accountNumber)){
@@ -28,45 +47,34 @@ public class EnterpriseAccountOwner extends AccountOwner{
         return result;
     }
 
-
-    //add a partner to the list of the Enterprise' partners
-    public void addPartner (EnterprisePartners partner){
-        if (partner != null){
+    public void addPartner (EnterprisePartner partner)
+    /* Adiciona um novo parceiro para a lista de EnterprisePartners */
+    {
+        if (partner != null) {
+            for (EnterprisePartner p : partners) {
+                if (p.getCNI().equals(partner.getCNI())) {
+                    Misc.log("Trying to add partner with ID " + p.getCNI() + " more than once.", Misc.LogType.ERROR);
+                    return;
+                }
+            }
             partners.add(partner);
-        }else{
-            System.out.println("Partner object does not exist");
         }
-
     }
+
     //remove a partner from the Enterprise list partners
-    public boolean removePartner (String partnerCNI){
+    public boolean removePartner (String partnerCNI) {
         boolean result = false;
         for (int i = 0; i < partners.size(); i++){
             if (partners.get(i).getCNI().equals(partnerCNI)){
                 partners.remove(partners.get(i));
-                return result;
+                result = true;
             }
         }
         return result;
     }
+
     //get a list of the Enterprise's partners
-    public ArrayList<EnterprisePartners> getPartners(){
+    public ArrayList<EnterprisePartner> getPartners(){
         return this.partners;
-    }
-
-    public EnterpriseAccountOwner(String ownerID,String name, String NIF, String address, ArrayList<EnterprisePartners> partners, ArrayList<EnterpriseAccount> accounts) {
-        super(ownerID, name, NIF, address);
-        this.partners = partners;
-        this.accounts = accounts;
-    }
-
-    public EnterpriseAccountOwner(String  ownerID, String name, String NIF, String address, ArrayList<EnterprisePartners> partners) {
-        this(ownerID, name, NIF, address, partners, new ArrayList<EnterpriseAccount>());
-    }
-    
-    public EnterpriseAccountOwner(String ownerID, String name, String NIF, String address) {
-        super(ownerID, name, NIF, address);
-        this.partners = new ArrayList<>();
-        this.accounts = new ArrayList<>();
     }
 }
