a347ab19450619d84b4249275240bf4f9dc14639
Carlos Monteiro <97047838+Carlos-MVM@users.noreply.github.com>
2022-07-21 20:00:48 -0100

    Update ClientsControllers: Adding create and refresh functions

--- Banku/src/main/java/com/groupnine/banku/controllers/ClientsController.java
+++ Banku/src/main/java/com/groupnine/banku/controllers/ClientsController.java
@@ -5,11 +5,18 @@ import com.groupnine.banku.businesslogic.*;
 import com.groupnine.banku.miscellaneous.ListUtils;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
 
 public class ClientsController {
+    private static ClientsController activeInstance;
     @FXML
     private Button dashboardBtn;
     @FXML
@@ -17,10 +24,126 @@ public class ClientsController {
     @FXML
     private TableView<EnterpriseAccountOwnerBean> enterpriseAccountOwnerTable;
 
+    private boolean particularAccountOwnerTableWasCreated = false;
+    private boolean enterpriseAccountOwnerTableWasCreated = false;
+
+    public static ClientsController getActiveInstance()
+        /* Retorna a instancia ativa e inicialiada dessa view. */
+    {
+        return activeInstance;
+    }
+
+    @FXML
+    protected void initialize()
+    {
+        createTables();
+        refreshTables();
+
+        /* Isto vai ser utilizado externamente para
+         * atualizar o estado dessa view.
+         * */
+        activeInstance = this;
+    }
+
+    private void createTables()
+        /* Cria todas as tabelas dessa view. */
+    {
+        createParticularAccountOwnerTable();
+        createEnterpriseAccountOwnerTable();
+    }
+
+    public void refreshTables()
+        /* Atualiza todas a tabelas. Este método é público pois
+         * pode ser utilizado externamente. */
+    {
+        refreshParticularAccountOwnerTable();
+        refreshEnterpriseAccountOwnerTable();
+    }
+
     @FXML
     protected void dashboardBtnOnClick()
     {
         BankuApp.getMainWindow().showDefaultView();
+    }
+
+
+    private void createParticularAccountOwnerTable()
+    {
+        particularAccountOwnerTable.getColumns().clear();
+        particularAccountOwnerTable.setEditable(false);
+
+        TableColumn<ClientsController.ParticularAccountOwnerBean, String> accId = new TableColumn<>("Id");
+        TableColumn<ClientsController.ParticularAccountOwnerBean, String>  accName = new TableColumn<>("Nome");
+        TableColumn<ClientsController.ParticularAccountOwnerBean, String>  accNIF = new TableColumn<>("NIF");
+        TableColumn<ClientsController.ParticularAccountOwnerBean, String>  accAddress = new TableColumn<>("Endereco");
+        TableColumn<ClientsController.ParticularAccountOwnerBean, String>  accNationality = new TableColumn<>("Nacionalidade");
+        TableColumn<ClientsController.ParticularAccountOwnerBean, Integer>  accNAccount = new TableColumn<>("#Contas");
+
+        accId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        accName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        accNIF.setCellValueFactory(new PropertyValueFactory<>("NIF"));
+        accAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        accNationality.setCellValueFactory(new PropertyValueFactory<>("nationality"));
+        accNAccount.setCellValueFactory(new PropertyValueFactory<>("accountNum"));
+
+        particularAccountOwnerTable.getColumns().addAll(accId, accName, accNIF, accAddress, accNationality, accNAccount);
+    }
+
+
+    private void createEnterpriseAccountOwnerTable()
+    {
+        enterpriseAccountOwnerTable.getColumns().clear();
+        enterpriseAccountOwnerTable.setEditable(false);
+
+        TableColumn<ClientsController.EnterpriseAccountOwnerBean, String> accId = new TableColumn<>("Id");
+        TableColumn<ClientsController.EnterpriseAccountOwnerBean, String>  accName = new TableColumn<>("Nome");
+        TableColumn<ClientsController.EnterpriseAccountOwnerBean, String>  accNIF = new TableColumn<>("NIF");
+        TableColumn<ClientsController.EnterpriseAccountOwnerBean, String>  accAddress = new TableColumn<>("Endereco");
+        TableColumn<ClientsController.EnterpriseAccountOwnerBean, Integer>  accNPartners = new TableColumn<>("Parceiros");
+        TableColumn<ClientsController.EnterpriseAccountOwnerBean, Integer>  accNAccount = new TableColumn<>("#Contas");
+
+        accId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        accName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        accNIF.setCellValueFactory(new PropertyValueFactory<>("NIF"));
+        accAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        accNPartners.setCellValueFactory(new PropertyValueFactory<>("partnersNum"));
+        accNAccount.setCellValueFactory(new PropertyValueFactory<>("accountsNum"));
+
+        enterpriseAccountOwnerTable.getColumns().addAll(accId, accName, accNIF, accAddress, accNPartners, accNAccount);
+    }
+
+    private void refreshParticularAccountOwnerTable()
+    {
+        if (!particularAccountOwnerTableWasCreated) {
+            createParticularAccountOwnerTable();
+        }
+
+        List<ParticularAccountOwner> ParticularOwnersList = BankAgency.getInstance().getParticularClientList();
+
+        ObservableList<ClientsController.ParticularAccountOwnerBean> data = FXCollections.observableArrayList();
+
+        for (ParticularAccountOwner accOwner : ParticularOwnersList) {
+            data.add(new ClientsController.ParticularAccountOwnerBean(accOwner));
+        }
+
+        particularAccountOwnerTable.setItems(data);
+    }
+
+    private void refreshEnterpriseAccountOwnerTable()
+    {
+        if (!enterpriseAccountOwnerTableWasCreated) {
+            createEnterpriseAccountOwnerTable();
+        }
+
+        List<EnterpriseAccountOwner> enterpriseOwnersList = BankAgency.getInstance().getEnterpriseClientList();
+
+        ObservableList<ClientsController.EnterpriseAccountOwnerBean> data = FXCollections.observableArrayList();
+
+        for (EnterpriseAccountOwner accOwner : enterpriseOwnersList) {
+            data.add(new ClientsController.EnterpriseAccountOwnerBean(accOwner));
+        }
+
+        enterpriseAccountOwnerTable.setItems(data);
     }
 
     public static abstract class AccountOwnerBean {
